9ff47a0c08a920d701db4fc7f5d7db0c55eca57e
ilyinae <112967353+ilyinae@users.noreply.github.com>
2022-12-11 23:58:25 +0300

    Add files via upload

---
+++ Calculator/main.py
@@ -0,0 +1,4 @@
+import calc_controller as cc
+
+if __name__ == '__main__' :
+    cc.calculate()
---
+++ Phone_Dictionary/main.py
@@ -0,0 +1,5 @@
+import dict_controller as dc
+
+if __name__ == '__main__' :
+    dc.init()
+    dc.manager()
